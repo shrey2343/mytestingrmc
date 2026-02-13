755358ac7f75fc383ff31d08341af859f62d9e73
shrey2343 <shreyn82@gmail.com>
2026-02-13 12:18:05 +0530

    popup add

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import Layout from "@/components/layout/Layout";
 import HeroSection from "@/components/home/HeroSection";
 import StatsSection from "@/components/home/StatsSection";
@@ -6,9 +7,28 @@ import IndustryCardsSection from "@/components/home/IndustryCardsSection";
 import HowWeWorkSection from "@/components/home/HowWeWorkSection";
 import TestimonialsSection from "@/components/home/TestimonialsSection";
 import CTASection from "@/components/home/CTASection";
-import  VideoTestimonialsSection from "@/components/home/VideoTestimonialsSection"
+import VideoTestimonialsSection from "@/components/home/VideoTestimonialsSection";
+import QuoteFormModal from "@/components/QuoteFormModal";
 
 const Index = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    // Check if modal has been shown in this session
+    const hasShownModal = sessionStorage.getItem('hasShownAutoModal');
+    
+    if (!hasShownModal) {
+      // Open modal after 5 seconds
+      const timer = setTimeout(() => {
+        setIsModalOpen(true);
+        sessionStorage.setItem('hasShownAutoModal', 'true');
+      }, 5000);
+
+      // Cleanup timer on unmount
+      return () => clearTimeout(timer);
+    }
+  }, []);
+
   return (
     <Layout>
       <HeroSection />
@@ -19,6 +39,9 @@ const Index = () => {
       <VideoTestimonialsSection/>
       <TestimonialsSection />
       <CTASection />
+      
+      {/* Auto-popup Modal */}
+      <QuoteFormModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </Layout>
   );
 };
